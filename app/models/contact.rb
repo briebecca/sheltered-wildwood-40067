33662487e203435aaa0a9b5a6f08d6785049c2a7
Sam W <sam@sam-we.com>
2020-12-08 06:37:13 -0800

    The cell phone should be a real cell phone number

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,5 @@
 class Contact < ApplicationRecord
+  validates_format_of :phone,
+    :with => /\(?[0-9]{3}\)?-[0-9]{3}-[0-9]{4}/,
+    :message => "- Phone numbers must be in xxx-xxx-xxxx format."
 end
