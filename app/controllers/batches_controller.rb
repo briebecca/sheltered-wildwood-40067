4bc23718af963ba9f2c75cfd2cab39f1a11252b0
Sam W <sam@sam-we.com>
2020-12-08 07:46:19 -0800

    Remove unused files

--- app/controllers/batches_controller.rb
+++ app/controllers/batches_controller.rb
@@ -1,24 +1,7 @@
 class BatchesController < ApplicationController
-  before_action :set_batch, only: [:show, :edit, :update, :destroy]
-
-  # GET /batches
-  # GET /batches.json
-  def index
-    @batches = Batch.all
-  end
-
-  # GET /batches/1
-  # GET /batches/1.json
-  def show
-  end
-
   # GET /batches/new
   def new
     @batch = Batch.new
-  end
-
-  # GET /batches/1/edit
-  def edit
   end
 
   # POST /batches
@@ -35,30 +18,6 @@ class BatchesController < ApplicationController
         format.html { render :new }
         format.json { render json: @batch.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  # PATCH/PUT /batches/1
-  # PATCH/PUT /batches/1.json
-  def update
-    respond_to do |format|
-      if @batch.update(batch_params)
-        format.html { redirect_to @batch, notice: 'Batch was successfully updated.' }
-        format.json { render :show, status: :ok, location: @batch }
-      else
-        format.html { render :edit }
-        format.json { render json: @batch.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /batches/1
-  # DELETE /batches/1.json
-  def destroy
-    @batch.destroy
-    respond_to do |format|
-      format.html { redirect_to batches_url, notice: 'Batch was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
